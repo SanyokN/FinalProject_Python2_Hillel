470bed0b2cc271d046fd6a7736909253a904e660
alnik <al.nikishin2000@gmail.com>
2024-08-09 14:45:13 +0200

    registration

--- web_router/web.py
+++ web_router/web.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, BackgroundTasks, Form
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 
+import dao
+from background_tasks.confirm_registration import confirm_registration
 from dao import get_all_trips, get_trip_by_id
 
 templates = Jinja2Templates(directory="templates")
@@ -26,5 +28,29 @@ def index(request: Request, query: str = Form(None)):
         "request": request,
         "trips": get_all_trips(50, 0, query),
         "title": "Main page",
+    }
+    return templates.TemplateResponse("index.html", context=context)
+
+
+@web_router.get("/register/", include_in_schema=True)
+@web_router.post("/register/", include_in_schema=True)
+def web_register(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    name: str = Form(None),
+    surname: str = Form(None),
+    email: str = Form(None),
+    password: str = Form(None),
+):
+    if request.method == "GET":
+        context = {"request": request, "title": "Register"}
+        return templates.TemplateResponse("registration.html", context=context)
+    created_user = dao.create_user(name, surname, email, password)
+    background_tasks.add_task(confirm_registration, created_user, request.base_url)
+    context = {
+        "request": request,
+        "title": "Register",
+        "trips": get_all_trips(limit=100, skip=0, name=""),
+        "user": created_user,
     }
     return templates.TemplateResponse("index.html", context=context)
